04fce1b2a040668aecc55ef1d06ba8af7c695c0b
Netkas <45276434+Netkas@users.noreply.github.com>
2019-08-09 18:01:45 -0400

    Removed unused constructor

--- src/tsa/tsa.php
+++ src/tsa/tsa.php
@@ -3,8 +3,10 @@
 
     namespace tsa;
 
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'Crypto.php');
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'Utilities.php');
 
-    class tsa
-    {
-
-    }
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'BadLengthException.php');
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'Base32DecodingException.php');
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'InvalidSecretException.php');
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'SecuredRandomProcessorNotFoundException.php');
